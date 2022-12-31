c6b88cffbc8730a2f4867831c165500e734dc804
Nelson <104731311+nelson9608@users.noreply.github.com>
2022-12-31 11:48:06 -0500

    Update web_search.py

--- scripts/web_search.py
+++ scripts/web_search.py
@@ -6,8 +6,8 @@ from simplebot.bot import DeltaBot, Replies
 from simplebot_instantview import prepare_html, session  # noqa
 
 
-@simplebot.filter(trylast=True)
-def search_filter(bot: DeltaBot, message: Message, replies: Replies) -> None:
+@simplebot.command
+def search(bot: DeltaBot, message: Message, replies: Replies) -> None:
     """Send me any text in private to search in the web."""
     if not replies.has_replies() and not message.chat.is_multiuser() and message.text:
         text, html = _search(bot.self_contact.addr, message.text)
