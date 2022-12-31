f68578431a7add2ec4c2fa9e0d29b9fb2604e27e
Nelson <104731311+nelson9608@users.noreply.github.com>
2022-12-31 12:11:55 -0500

    Update web_search.py

--- scripts/web_search.py
+++ scripts/web_search.py
@@ -14,7 +14,7 @@ def search2(bot: DeltaBot, payload: str, message: Message, replies: Replies) -> 
         replies.add(text=text or "Search results", html=html, quote=message)
 
 
-def search2(bot_addr: str, query: str) -> tuple:
+def _search2_cmd(1, bot, payload, message, replies) -> tuple:
     with session.get(f"https://duckduckgo.com/lite?q={quote_plus(query)}") as resp:
         resp.raise_for_status()
         return prepare_html(bot_addr, resp.url, resp.text)
