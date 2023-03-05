854fa86fd04bca9c776fef216b543b108833a130
Nelson <104731311+nelson9608@users.noreply.github.com>
2023-03-05 13:00:53 -0500

    Add files via upload

---
+++ scripts/malavida.py
@@ -0,0 +1,20 @@
+from urllib.parse import quote_plus
+
+import simplebot
+from deltachat import Message
+from simplebot.bot import DeltaBot, Replies
+from simplebot_instantview import prepare_html, session  # noqa
+
+
+@simplebot.command
+def malavida(bot: DeltaBot, message: Message, replies: Replies) -> None:
+    """Send me the name of the APK you want to search in malavida"""
+    if not replies.has_replies() and not message.chat.is_multiuser() and message.text:
+        text, html = _search(bot.self_contact.addr, message.text)
+        replies.add(text=text or "Search results", html=html, quote=message)
+
+
+def _search2_cmd(1, bot, payload, message, replies) -> tuple:
+    with session.get(f"https://www.malavida.com/es/android/s/{quote_plus(query)}") as resp:
+        resp.raise_for_status()
+        return prepare_html(bot_addr, resp.url, resp.text)
