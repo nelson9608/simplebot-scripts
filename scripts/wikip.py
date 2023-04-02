2ec1fb6cd516b6d20a675ec5132f9372fddac3b4
Nelson <104731311+nelson9608@users.noreply.github.com>
2023-04-02 06:16:23 -0400

    Update wikip.py

--- scripts/wikip.py
+++ scripts/wikip.py
@@ -6,7 +6,7 @@ from simplebot.bot import DeltaBot, Replies
 from simplebot_instantview import prepare_html, session  # noqa
 
 @simplebot.command()
-def search_filter(bot: DeltaBot, message: Message, replies: Replies) -> 
+def wiki(bot: DeltaBot, message: Message, replies: Replies) -> 
 None:
      """Send me any text in private to search in Wikipedia."""
      if not replies.has_replies() and not message.chat.is_multiuser() and 
