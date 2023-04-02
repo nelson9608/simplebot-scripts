665d95260d87034efc8cccd087c3378cda28921d
Nelson <104731311+nelson9608@users.noreply.github.com>
2023-04-02 15:36:34 -0400

    Update tvseries.py

--- scripts/tvseries.py
+++ scripts/tvseries.py
@@ -7,23 +7,15 @@ from simplebot_instantview import prepare_html, session  # noqa
 
 
 @simplebot.command()
-def serie(bot: DeltaBot, message: Message, replies: Replies) -> None:
+def imdb(bot: DeltaBot, message: Message, replies: Replies) -> None:
     """Send me the name of a TV series and I will send you a synopsis of it."""
     if not replies.has_replies() and not message.chat.is_multiuser() and message.text:
         text, html = _search(bot.self_contact.addr, message.text)
         replies.add(text=text or "Search results", html=html, quote=message)
-      
+
+
 def _search(bot_addr: str, query: str) -> tuple:
-     with session.get(f"https://www.imdb.com/find?q={quote_plus(query)}&s=tt&ttype=ft&ref_=fn_ft") as resp:
+     query = query.replace("/wiki", "") 
+     with session.get(f"https://www.imdb.com/find?q={quote_plus(query)}") as resp:
          resp.raise_for_status()
-         soup = BeautifulSoup(resp.text, "html.parser")
-         result = soup.find("td", class_="result_text")
-         if result:
-             link = result.find("a")["href"]
-             with session.get(f"https://www.imdb.com{link}") as resp:
-                 resp.raise_for_status()
-                 soup = BeautifulSoup(resp.text, "html.parser")
-                 summary = soup.find("div", class_="summary_text").get_text(strip=True)
-                 return prepare_html(bot_addr, f"https://www.imdb.com{link}", summary)
-         else:
-             return prepare_html(bot_addr, resp.url, "No se encontraron resultados en IMDb.")
+         return prepare_html(bot_addr, resp.url, resp.text)
