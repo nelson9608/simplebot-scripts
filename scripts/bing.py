02960829a06a9a46e28554ca22278b9217845ee1
Nelson <104731311+nelson9608@users.noreply.github.com>
2023-04-04 15:42:39 -0400

    Update bing.py

--- scripts/bing.py
+++ scripts/bing.py
@@ -1,37 +1,104 @@
-from urllib.parse import quote_plus
-import os
-import requests
-from bs4 import BeautifulSoup
+import functools
+import io
+import mimetypes
+import random
+import re
+from typing import Generator
+from urllib.parse import quote, quote_plus
 
+import bs4
+import requests
 import simplebot
 from deltachat import Message
 from simplebot.bot import DeltaBot, Replies
-from simplebot_instantview import prepare_html, session  # noqa
 
-@simplebot.command()
-def bing(bot: DeltaBot, message: Message, replies: Replies) -> None:
-      """Send me any text in private to search in Bing"""
-      if message.is_from_me() and message.is_private():
-         urls = _search(message.text)
-         download_images(urls)
-         chat.send_text("Images downloaded", reply_to=message)
+session = requests.Session()
+session.headers.update(
+    {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0"
+    }
+)
+session.request = functools.partial(session.request, timeout=15)  # type: ignore
 
-def _search(query: str) -> list:
-     query = query.replace("/bing", "")
-     with requests.Session() as session:
-         resp = session.get(f"https://www.bing.com/images/search?q={quote_plus(query)}&form=HDRSC2&first=1&tsc=ImageBasicHover")
+
+@simplebot.command
+def image(bot: DeltaBot, payload: str, message: Message, replies: Replies) -> None:
+    """Get an image based on the given text.
+
+    Example:
+    /image cats and dogs
+    """
+    _image_cmd(1, bot, payload, message, replies)
+
+
+@simplebot.command
+def image5(bot: DeltaBot, payload: str, message: Message, replies: Replies) -> None:
+    """Search for images, returns up to five results.
+
+    Example:
+    /image5 roses
+    """
+    _image_cmd(5, bot, payload, message, replies)
+
+
+def _image_cmd(
+    img_count: int, bot: DeltaBot, payload: str, message: Message, replies: Replies
+) -> None:
+    if not payload:
+        replies.add(text="❌ No text given", quote=message)
+        return
+    imgs = img_count
+    for filename, data in _get_images(bot, payload):
+        replies.add(filename=filename, bytefile=io.BytesIO(data))
+        imgs -= 1
+        if imgs <= 0:
+            break
+    if imgs == img_count:
+        replies.add(text="❌ No results", quote=message)
+
+
+def _get_images(bot: DeltaBot, query: str) -> Generator:
+    img_providers = [_google_imgs, _startpage_imgs, _dogpile_imgs]
+    img_providers_low = [_alphacoders, _unsplash, _everypixel]
+    while img_providers:
+        provider = random.choice(img_providers)
+        img_providers.remove(provider)
+        try:
+            bot.logger.debug("Trying %s", provider)
+            for img_url in provider(query):
+                with session.get(img_url) as resp:
+                    resp.raise_for_status()
+                    filename = "image" + (get_extension(resp) or ".jpg")
+                    yield filename, resp.content
+        except Exception as err:
+            bot.logger.exception(err)
+        if not img_providers and img_providers_low:
+            img_providers.extend(img_providers_low)
+            img_providers_low.clear()
+
+
+def _bing_imgs(query: str) -> set:
+     url = f"https://www.bing.com/images/search?q={quote_plus(query)}"
+     with session.get(url) as resp:
          resp.raise_for_status()
-         soup = BeautifulSoup(resp.text, 'html.parser')
-         urls = []
-         for img in soup.find_all('img'):
-             url = img.get('src')
-             if url.startswith('http'):
-                 urls.append(url)
-         return urls
+         soup = bs4.BeautifulSoup(resp.text, "html5lib")
+     links = set()
+     for div in soup("div", {"class": "imgpt"}):
+         img = div.find("img")
+         if img["src"].startswith("data:"):
+             continue
+         links.add(img["src"])
+     return links
 
-def download_images(urls: list):
-     for url in urls:
-         response = requests.get(url)
-         filename = os.path.basename(url)
-         with open(filename, 'wb') as f:
-             f.write(response.content)
+def get_extension(resp: requests.Response) -> str:
+    disp = resp.headers.get("content-disposition")
+    if disp is not None and re.findall("filename=(.+)", disp):
+        fname = re.findall("filename=(.+)", disp)[0].strip('"')
+    else:
+        fname = resp.url.split("/")[-1].split("?")[0].split("#")[0]
+    if "." in fname:
+        ext = "." + fname.rsplit(".", maxsplit=1)[-1]
+    else:
+        ctype = resp.headers.get("content-type", "").split(";")[0].strip().lower()
+        ext = mimetypes.guess_extension(ctype) or ""
+    return ext
