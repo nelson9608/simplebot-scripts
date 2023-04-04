43cb8e9424f0f299e982e17eb4c85da6f408cb5c
Nelson <104731311+nelson9608@users.noreply.github.com>
2023-04-04 13:49:24 -0400

    Update bing.py

--- scripts/bing.py
+++ scripts/bing.py
@@ -10,14 +10,22 @@ from simplebot_instantview import prepare_html, session  # noqa
 
 @simplebot.command()
 def bing(bot: DeltaBot, message: Message, replies: Replies) -> None:
-     """Send me any text in private to search in Bing"""
-     if not replies.has_replies() and not message.chat.is_multiuser() and message.text:
-         urls = _search(message.text)
-         download_images(urls)
-         replies.add(text="Images downloaded", quote=message)
+      """Send me any text in private to search in Bing"""
+      if not replies.has_replies() and not message.chat.is_multiuser() and message.text:
+          urls = _search(message.text)
+          download_images(urls)
+          replies.add(text="Images downloaded", quote=message)
 
 def _search(query: str) -> list:
-     query = query.replace("/bing", "") 
-     with session.get(f"https://www.bing.com/images/search?q={quote_plus(query)}&form=HDRSC2&first=1&tsc=ImageBasicHover") as resp:
-         resp.raise_for_status()
-         soup = BeautifulSoup(resp.text, 'html')
+      query = query.replace("/bing", "")
+      with session.get(f"https://www.bing.com/images/search?q={quote_plus(query)}&form=HDRSC2&first=1&tsc=ImageBasicHover") as resp:
+          resp.raise_for_status()
+          soup = BeautifulSoup(resp.text, 'html.parser')
+          urls = []
+          for img in soup.find_all('img'):
+              url = img.get('src')
+              if url.startswith('http'):
+                  urls.append(url)
+          return urls
+
+def download_images(urls: list)
