231ab122d06e0b5ed9c8243e29616e878100f45d
Nelson <104731311+nelson9608@users.noreply.github.com>
2023-04-04 00:01:00 -0400

    Update bing.py

--- scripts/bing.py
+++ scripts/bing.py
@@ -11,8 +11,7 @@ from simplebot_instantview import prepare_html, session  # noqa
 @simplebot.command()
 def bing(bot: DeltaBot, message: Message, replies: Replies) -> None:
      """Send me any text in private to search in Bing"""
-     if not replies.has_replies() and not message.chat.is_multiuser() and 
-message.text:
+     if not replies.has_replies() and not message.chat.is_multiuser() and message.text:
          urls = _search(message.text)
          download_images(urls)
          replies.add(text="Images downloaded", quote=message)
