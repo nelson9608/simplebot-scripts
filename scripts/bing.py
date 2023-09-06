0e72d34fb42be091cd69b890349b9dd4cdf16f4e
Nelson <104731311+nelson9608@users.noreply.github.com>
2023-09-05 23:52:33 -0400

    Update bing.py

--- scripts/bing.py
+++ scripts/bing.py
@@ -1,104 +1,46 @@
-import functools
-import io
-import mimetypes
-import random
-import re
-from typing import Generator
-from urllib.parse import quote, quote_plus
-
-import bs4
 import requests
+from bs4 import BeautifulSoup
 import simplebot
-from deltachat import Message
 from simplebot.bot import DeltaBot, Replies
 
-session = requests.Session()
-session.headers.update(
-    {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0"
-    }
-)
-session.request = functools.partial(session.request, timeout=15)  # type: ignore
+@simplebot.command
+def bing_image(bot: DeltaBot, payload: str, message: Message, replies: Replies) -> None:
+    """Obtener una imagen basada en el texto dado.
 
+    Ejemplo:
+    /bing_image gatos y perros
+    """
+    _bing_image_cmd(1, bot, payload, message, replies)
 
 @simplebot.command
-def image(bot: DeltaBot, payload: str, message: Message, replies: Replies) -> None:
-    """Get an image based on the given text.
+def bing_image5(bot: DeltaBot, payload: str, message: Message, replies: Replies) -> None:
+    """Buscar imágenes en Bing y devolver hasta cinco resultados.
 
-    Example:
-    /image cats and dogs
+    Ejemplo:
+    /bing_image5 rosas
     """
-    _image_cmd(1, bot, payload, message, replies)
+    _bing_image_cmd(5, bot, payload, message, replies)
 
-
-@simplebot.command
-def image5(bot: DeltaBot, payload: str, message: Message, replies: Replies) -> None:
-    """Search for images, returns up to five results.
-
-    Example:
-    /image5 roses
-    """
-    _image_cmd(5, bot, payload, message, replies)
-
-
-def _image_cmd(
-    img_count: int, bot: DeltaBot, payload: str, message: Message, replies: Replies
-) -> None:
+def _bing_image_cmd(img_count: int, bot: DeltaBot, payload: str, message: Message, replies: Replies) -> None:
     if not payload:
-        replies.add(text="❌ No text given", quote=message)
+        replies.add(text="❌ No se proporcionó texto", quote=message)
         return
     imgs = img_count
-    for filename, data in _get_images(bot, payload):
+    for filename, data in _get_bing_images(bot, payload):
         replies.add(filename=filename, bytefile=io.BytesIO(data))
         imgs -= 1
         if imgs <= 0:
             break
     if imgs == img_count:
-        replies.add(text="❌ No results", quote=message)
+        replies.add(text="❌ No hay resultados", quote=message)
 
-
-def _get_images(bot: DeltaBot, query: str) -> Generator:
-    img_providers = [_google_imgs, _startpage_imgs, _dogpile_imgs]
-    img_providers_low = [_alphacoders, _unsplash, _everypixel]
-    while img_providers:
-        provider = random.choice(img_providers)
-        img_providers.remove(provider)
-        try:
-            bot.logger.debug("Trying %s", provider)
-            for img_url in provider(query):
-                with session.get(img_url) as resp:
-                    resp.raise_for_status()
-                    filename = "image" + (get_extension(resp) or ".jpg")
-                    yield filename, resp.content
-        except Exception as err:
-            bot.logger.exception(err)
-        if not img_providers and img_providers_low:
-            img_providers.extend(img_providers_low)
-            img_providers_low.clear()
-
-
-def _bing_imgs(query: str) -> set:
-     url = f"https://www.bing.com/images/search?q={quote_plus(query)}"
-     with session.get(url) as resp:
-         resp.raise_for_status()
-         soup = bs4.BeautifulSoup(resp.text, "html5lib")
-     links = set()
-     for div in soup("div", {"class": "imgpt"}):
-         img = div.find("img")
-         if img["src"].startswith("data:"):
-             continue
-         links.add(img["src"])
-     return links
-
-def get_extension(resp: requests.Response) -> str:
-    disp = resp.headers.get("content-disposition")
-    if disp is not None and re.findall("filename=(.+)", disp):
-        fname = re.findall("filename=(.+)", disp)[0].strip('"')
-    else:
-        fname = resp.url.split("/")[-1].split("?")[0].split("#")[0]
-    if "." in fname:
-        ext = "." + fname.rsplit(".", maxsplit=1)[-1]
-    else:
-        ctype = resp.headers.get("content-type", "").split(";")[0].strip().lower()
-        ext = mimetypes.guess_extension(ctype) or ""
-    return ext
+def _get_bing_images(bot: DeltaBot, query: str):
+    url = f"https://www.bing.com/images/search?q={query}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    images = soup.find_all("img", class_="mimg")
+    for image in images[:num_results]:
+        image_url = image["src"]
+        image_data = requests.get(image_url).content
+        filename = f"{query}_{num_results}.jpg"
+        yield filename, image_data
