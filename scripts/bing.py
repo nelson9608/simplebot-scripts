2f21898b9b5e5689bc5e7f293d15cbb1fc34c4cb
Nelson <104731311+nelson9608@users.noreply.github.com>
2023-09-09 16:20:38 -0400

    Update bing.py

--- scripts/bing.py
+++ scripts/bing.py
@@ -1,46 +1,22 @@
+import simplebot
 import requests
 from bs4 import BeautifulSoup
-import simplebot
-from simplebot.bot import DeltaBot, Replies
 
 @simplebot.command
-def bing_image(bot: DeltaBot, payload: str, message: Message, replies: Replies) -> None:
-    """Obtener una imagen basada en el texto dado.
-
-    Ejemplo:
-    /bing_image gatos y perros
-    """
-    _bing_image_cmd(1, bot, payload, message, replies)
-
-@simplebot.command
-def bing_image5(bot: DeltaBot, payload: str, message: Message, replies: Replies) -> None:
-    """Buscar imágenes en Bing y devolver hasta cinco resultados.
-
-    Ejemplo:
-    /bing_image5 rosas
-    """
-    _bing_image_cmd(5, bot, payload, message, replies)
-
-def _bing_image_cmd(img_count: int, bot: DeltaBot, payload: str, message: Message, replies: Replies) -> None:
-    if not payload:
-        replies.add(text="❌ No se proporcionó texto", quote=message)
+def bing_images(message, replies):
+    """Busca imágenes en Bing y devuelve las primeras 5 imágenes."""
+    query = message.text.partition(' ')[2]
+    if not query:
+        replies.add(text='Por favor, proporciona una consulta de búsqueda.')
         return
-    imgs = img_count
-    for filename, data in _get_bing_images(bot, payload):
-        replies.add(filename=filename, bytefile=io.BytesIO(data))
-        imgs -= 1
-        if imgs <= 0:
-            break
-    if imgs == img_count:
-        replies.add(text="❌ No hay resultados", quote=message)
 
-def _get_bing_images(bot: DeltaBot, query: str):
-    url = f"https://www.bing.com/images/search?q={query}"
+    url = f'https://www.bing.com/images/search?q={query}'
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    images = soup.find_all("img", class_="mimg")
-    for image in images[:num_results]:
-        image_url = image["src"]
-        image_data = requests.get(image_url).content
-        filename = f"{query}_{num_results}.jpg"
-        yield filename, image_data
+    soup = BeautifulSoup(response.text, 'html.parser')
+    images = soup.find_all('img', class_='mimg')
+    for image in images[:5]:
+        image_url = image['src']
+        image_response = requests.get(image_url)
+        with open('bing_image.jpg', 'wb') as f:
+            f.write(image_response.content)
+        replies.add(filename='bing_image.jpg', caption=f'Imagen de {query} en Bing')
